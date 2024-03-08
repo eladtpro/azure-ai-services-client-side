2a5a9b086853fad17eb3b246fdb7560d304ca780
eladtpro <eladt.pro@gmail.com>
2024-03-08 08:59:17 +0200

    @babel/plugin-proposal-private-property-in-object

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
 // import React from 'react';
-// import { createRoot } from 'react-dom/client';
+// import ReactDOM from 'react-dom';
 // import App from './App';
-// const container = document.getElementById('root');
 
-// const root = createRoot(container);
-// root.render(
+// ReactDOM.render(
 //   <React.StrictMode>
 //     <App />
-//   </React.StrictMode>
+//   </React.StrictMode>,
+//   document.getElementById('root')
 // );
+
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+const container = document.getElementById('root');
+
+const root = createRoot(container);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
